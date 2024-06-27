7f6146d628338fa7d7b88bc3f4efac70b0a97669
VitaliyBaranivskyy <t0677260269@gmail.com>
2024-06-27 20:00:04 +0300

    1

---
+++ js/task-2.js
@@ -0,0 +1,29 @@
+class Storage {
+  #items;
+  constructor(array) {
+    this.#items = array;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    for (let index = 0; index < this.#items.length; index += 1) {
+      if (this.#items[index] === itemToRemove) this.#items.splice(index,1);
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
